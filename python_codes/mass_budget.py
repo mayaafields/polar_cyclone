8597c8ab3e7e04c6475911f7eddcfd6a09406485
Maya A Fields <mafields@umich.edu>
2022-05-18 10:27:47 -0400

    added the combination of the density variations and the mass flux from equation 3 in overleaf

--- python_codes/mass_budget.py
+++ python_codes/mass_budget.py
@@ -20,6 +20,12 @@ ntime, nx1, nx2 = rho_gas.shape
 rho_0 = rho_gas[0,:,0]
 density_variations = rho_gas - rho_0.reshape((1,nx1,1))
 
+
+horizontally_averaged_density_var = mean(density_variations, axis =2)
+
+
+heat_rate= horizontally_averaged_density_var + mass_flux
+
 #Plotting Density Variations
 
 pavg = mean(pressure, axis = (0,2))
@@ -27,7 +33,7 @@ Pressure, Time = meshgrid(pavg,time)
 
 fig,ax=subplots(1,1)
 
-contour_plot_density_var = ax.contourf(Time, Pressure, mean(density_variations, axis =2))
+contour_plot_density_var = ax.contourf(Time, Pressure,horizontally_averaged_density_var)
 title(r'$\frac{\delta \rho}{\delta t}$')
 ax.set_xlabel(r'Time(s)',fontsize = 12)
 ax.set_ylabel(r'Pressure',fontsize = 12)
@@ -47,5 +53,21 @@ ax.set_ylim(max(pavg), min(pavg))
 ax.set_yscale('log')
 fig.colorbar(contour_plot_mass_flux)
 savefig('contour_plot_mass_flux.png')
+
+
+#Plotting the Heating Rate due to condensation? 
+# unsure what the term is called without the Latent heat
+
+fig,ax= subplots(1,1)
+contour_plot_heat_rate = ax.contourf(Time, Pressure, heat_rate)
+title(r'$-<\rho \dot{q_c}>$')
+ax.set_xlabel(r'Time(s)',fontsize = 12)
+ax.set_ylabel(r'Pressure',fontsize = 12)
+ax.set_ylim(max(pavg), min(pavg))
+ax.set_yscale('log')
+fig.colorbar(contour_plot_heat_rate)
+savefig('contour_plot_heat_rate.png')
+
+
 
 show()
