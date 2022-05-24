4669363123ed8a434670507a91bc307c13bad233
Maya A Fields <mafields@umich.edu>
2022-05-24 12:33:09 -0400

    updated syntax

--- python_codes/mass_budget.py
+++ python_codes/mass_budget.py
@@ -1,4 +1,4 @@
-#Mass Budget Python Scrip
+#Mass Budget Python Script
 
 
 
@@ -12,61 +12,64 @@ data = Dataset(fname, 'r')
 
 #Defining Variables
 time = data['time']
-rho_gas = data['rho'][:,:,:,0]
-pressure = data['press'][:,:,:,0]
+rho = data['rho'][:,:,:,0]
+pres = data['press'][:,:,:,0]
 mass_flux = data['v1rho'][:,:]
-ntime, nx1, nx2 = rho_gas.shape
+ntime, nx1, nx2 = rho.shape
 
-rho_0 = rho_gas[0,:,0]
-density_variations = rho_gas - rho_0.reshape((1,nx1,1))
-
-
-horizontally_averaged_density_var = mean(density_variations, axis =2)
+rho_0 = rho[0,:,0]
+density_variations = rho - rho_0.reshape((1,nx1,1))
 
 
-heat_rate= horizontally_averaged_density_var + mass_flux
+rhoa_avg = mean(density_variations, axis =2)
+
+
+#heat_rate= rhoa_avg + mass_flux
+# this formula is incorrect 
 
 #Plotting Density Variations
 
-pavg = mean(pressure, axis = (0,2))
-Pressure, Time = meshgrid(pavg,time)
+p_avg = mean(pres, axis = (0,2))
+pres, time = meshgrid(p_avg,time)
 
 fig,ax=subplots(1,1)
 
-contour_plot_density_var = ax.contourf(Time, Pressure,horizontally_averaged_density_var)
+h = ax.contourf(time, pres,rhoa_avg)
 title(r'$\frac{\delta \rho}{\delta t}$')
 ax.set_xlabel(r'Time(s)',fontsize = 12)
 ax.set_ylabel(r'Pressure',fontsize = 12)
-ax.set_ylim(max(pavg), min(pavg))
+ax.set_ylim(max(p_avg), min(p_avg))
 ax.set_yscale('log')
-fig.colorbar(contour_plot_density_var)
-savefig('contour_plot_density.png')
+fig.colorbar(h)
+savefig('contour_plot_density.png', bbox_inches = 'tight')
 
 #Plotting the Vertical Mass Flux
 
 fig,ax= subplots(1,1)
-contour_plot_mass_flux = ax.contourf(Time, Pressure, mass_flux)
+h = ax.contourf(time, pres, mass_flux)
 title(r'$\frac{\delta(w \rho)}{\delta z}$')
 ax.set_xlabel(r'Time(s)',fontsize = 12)
 ax.set_ylabel(r'Pressure',fontsize = 12)
-ax.set_ylim(max(pavg), min(pavg))
+ax.set_ylim(max(p_avg), min(p_avg))
 ax.set_yscale('log')
-fig.colorbar(contour_plot_mass_flux)
-savefig('contour_plot_mass_flux.png')
+fig.colorbar(h)
+savefig('contour_plot_mass_flux.png', bbox_inches = 'tight')
 
 
 #Plotting the Heating Rate due to condensation? 
 # unsure what the term is called without the Latent heat
+# formula above is incorrect. Will replot this when I update my understanding. 
 
-fig,ax= subplots(1,1)
-contour_plot_heat_rate = ax.contourf(Time, Pressure, heat_rate)
-title(r'$-<\rho \dot{q_c}>$')
-ax.set_xlabel(r'Time(s)',fontsize = 12)
-ax.set_ylabel(r'Pressure',fontsize = 12)
-ax.set_ylim(max(pavg), min(pavg))
-ax.set_yscale('log')
-fig.colorbar(contour_plot_heat_rate)
-savefig('contour_plot_heat_rate.png')
+
+#fig,ax= subplots(1,1)
+#h = ax.contourf(Time, Pressure, heat_rate)
+#title(r'$-<\rho \dot{q_c}>$')
+#ax.set_xlabel(r'Time(s)',fontsize = 12)
+#ax.set_ylabel(r'Pressure',fontsize = 12)
+#ax.set_ylim(max(p_avg), min(p_avg))
+#ax.set_yscale('log')
+#fig.colorbar(h)
+#savefig('contour_plot_heat_rate.png', bbox_inches = 'tight')
 
 
 
