8d04e789cf415e0ddec0d1658aa74086dde0e872
Maya A Fields <mafields@umich.edu>
2022-06-02 11:24:07 -0400

    updated mass_budget code for mass flux and density variations

--- python_codes/mass_budget.py
+++ python_codes/mass_budget.py
@@ -24,34 +24,22 @@ print(deltime1, deltime2)
 
 time_nc = time[280:500] # time inbetween convection events
 
-#Creating Levels
-#Seem to be getting results where the 1 bar and 170 bar density levels seems similar. This doesn't make sense to me since the 2nd level is different. The 
-
 rho_lev1 = rho_avg[:,0] #170 bar
 rho_lev1_nc = rho_lev1[280:500]
-rho_lev2 = rho_avg[:,24] #85 bar
-rho_lev2_nc = rho_lev2[280:500]
-rho_lev3 = rho_avg[:,47] #1 bar
-rho_lev3_nc = rho_lev3[280:500]
 
 # Plotting 1D density versus time 
 fig,ax=subplots(1,1)
 
 plot(time, rho_lev1, label = 'level 1 (170 bar)')
-#plot(time, rho_lev2, label = 'level 2 (85 bar)')
-#plot(time, rho_lev3, label = 'level 3 (1 bar)')
 title(r'Density versus Time')
 ax.set_xlabel(r'Time(s)',fontsize = 12)
 ax.set_ylabel(r'$\rho$',fontsize = 12)
 legend()
 savefig('density_timeseries.png',bbox_inches= 'tight')
 
-
 fig,ax=subplots(1,1)
 
 plot(time_nc, rho_lev1_nc, label = 'level 1 (170 bar)')
-#plot(time_nc, rho_lev2_nc, label ='level 2 (85 bar)')
-#plot(time_nc, rho_lev3_nc, label ='level 3 (1 bar)')
 title(r'Density versus Time')
 ax.set_xlabel(r'Time(s)',fontsize = 12)
 ax.set_ylabel(r'$\rho$',fontsize = 12)
@@ -59,65 +47,58 @@ legend()
 savefig('noconvec_density_timeseries.png',bbox_inches= 'tight')
 
 #Linear Fits
-print(rho_lev3_nc.shape)
+
+beta = np.zeros(48)
+for i in range(nx1):
+    rho_level = rho_avg[:,i]
+    rho_level_nc = rho_level[280:500]
+    linfit = polyfit(time_nc, rho_level_nc,1)
+    beta[i] = linfit[0]
+
+z = arange(0,48,1)
+
+fig,ax=subplots(1,1)
+plot(beta,z)
+title(r'$\frac{\delta \rho}{\delta t}$')
+ax.set_xlabel(r'$\frac{\delta \rho}{\delta t}$',fontsize = 12)
+ax.set_ylabel('z',fontsize = 12)
+savefig('noconvec_density_variation.png',bbox_inches= 'tight')
 
 #Coefficents
 lev1_linfit = polyfit(time_nc,rho_lev1_nc,1)
-lev2_linfit = polyfit(time_nc,rho_lev2_nc,1)
-lev3_linfit = polyfit(time_nc,rho_lev3_nc,1)
 
 #Lines
 y1 = lev1_linfit[0]*time_nc + lev1_linfit[1]
-y2 = lev1_linfit[0]*time_nc + lev2_linfit[1]
-y3 = lev3_linfit[0]*time_nc + lev3_linfit[1]
-
 
 fig,ax = subplots(1,1)
 
 plot(time_nc, y1, label = 'level 1 (170 bar)')
-#plot(time_nc, y2, label ='level 2 (85 bar)')
-#plot(time_nc, y3, label ='level 3 (1 bar)')
 title(r'Linear Fits of Density versus Time')
 ax.set_xlabel(r'Time(s)',fontsize = 12)
 ax.set_ylabel(r'$\rho$',fontsize = 12)
 legend()
 savefig('linfits_of_density_timeseries.png',bbox_inches= 'tight')
 
-##Old Method
-
-#Plotting Density Variations
-
-#p_avg = mean(pres, axis = (0,2))
-#pres, time = meshgrid(p_avg,time)
-
-#fig,ax=subplots(1,1)
-
-#h = ax.contourf(time, pres,rhoa_avg)
-#title(r'$\frac{\delta \rho}{\delta t}$')
-#ax.set_xlabel(r'Time(s)',fontsize = 12)
-#ax.set_ylabel(r'Pressure',fontsize = 12)
-#ax.set_ylim(max(p_avg), 10**6)
-#ax.set_yscale('log')
-#fig.colorbar(h)
-#savefig('contour_plot_density_deep.png', bbox_inches = 'tight')
-
 #Plotting the Vertical Mass Flux
-mass_flux_lev1 =mass_flux[:,0] #170 bar
-mass_flux_lev1_nc = mass_flux_lev1[280:500]
-mass_flux_lev2 =mass_flux[:,24] #85 bar
-mass_flux_lev2_nc = mass_flux_lev1[280:500]
-mass_flux_lev3 =mass_flux[:,47] #1 bar
-mass_flux_lev3_nc = mass_flux_lev1[280:500]
+mass_flux_nc = mass_flux[280:500,:]
+mass_flux_avg = mean(mass_flux_nc, axis = 0)
+print(mass_flux_avg)
+
+fig,ax= subplots(1,1)
+plot(mass_flux_avg,z)
+title(r'$(w \rho)$')
+ax.set_ylabel('z',fontsize = 12)
+ax.set_xlabel(r'Mass Flux ($\frac{kg}{s \cdot m^2}$)',fontsize = 12)
+savefig('mass_flux_nc.png', bbox_inches = 'tight')
 
 
 fig,ax= subplots(1,1)
-plot(time_nc, mass_flux_lev1_nc, label = 'level 1 (170 bar)')
-plot(time_nc, mass_flux_lev2_nc, label ='level 2 (85 bar)')
-plot(time_nc, mass_flux_lev3_nc, label ='level 3 (1 bar)')
-title(r'$\frac{\delta(w \rho)}{\delta z}$')
-ax.set_xlabel(r'Time(s)',fontsize = 12)
-ax.set_ylabel(r'Mass Flux ($\frac{kg}{s \cdot m^2}$)',fontsize = 12)
-savefig('mass_flux_nc.png', bbox_inches = 'tight')
+plot(mass_flux_avg,z, label = r'$(w \rho)$')
+plot(beta,z ,label = r'$ \frac{\delta \rho}{\delta t}$')
+ax.set_ylabel('z',fontsize = 12)
+#ax.set_xlabel(r'Mass Flux ($\frac{kg}{s \cdot m^2}$)',fontsize = 12)
+legend()
+savefig('mf_rhovar_nc.png', bbox_inches = 'tight')
 
 
 #Plotting the Heating Rate due to condensation? 
